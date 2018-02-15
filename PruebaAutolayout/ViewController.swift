c2f3818186b5c5af3c777bed50c42d46d192db77
Francisco Molina <frangarmo@Franciscos-Mac.local>
2018-02-15 08:32:04 +0100

    Initial Commit

---
+++ PruebaAutolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaAutolayout
+//
+//  Created by Francisco García Molina on 15/2/18.
+//  Copyright © 2018 Francisco García Molina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
